ec38d3bd0beaf405959f61fa20f21031e2231284
github xxbbxx825 <xxbbxx825@gmail.com>
2023-11-06 19:11:10 +0900

    Finished up to Chapter 14

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -5,12 +5,16 @@ import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroi
 import Link from 'next/link'
 import { Button } from '@/app/ui/button'
 import { updateInvoice } from '@/app/lib/actions'
+import { useFormState } from 'react-dom'
 
 export default function EditInvoiceForm({ invoice, customers }: { invoice: InvoiceForm; customers: CustomerField[] }) {
+  const initialState = { message: null, errors: {} }
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id)
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState)
+
   console.log('invoice.id:', invoice.id)
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Invoice ID */}
         <input type="hidden" name="id" value={invoice.id} />
@@ -25,6 +29,7 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
               name="customerId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -37,6 +42,13 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          {state.errors?.customerId ? (
+            <div id="customer-error" aria-live="polite" className="mt-2 text-sm text-red-500">
+              {state.errors.customerId.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
 
         {/* Invoice Amount */}
@@ -53,10 +65,18 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          {state.errors?.amount ? (
+            <div id="amount-error" aria-live="polite" className="mt-2 text-sm text-red-500">
+              {state.errors.amount.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
 
         {/* Invoice Status */}
@@ -66,7 +86,7 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
           </label>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
-              <div className="flex items-center">
+              <div className="flex items-center" aria-describedby="status-error">
                 <input
                   id="pending"
                   name="status"
@@ -100,6 +120,13 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
               </div>
             </div>
           </div>
+          {state.errors?.status ? (
+            <div id="status-error" aria-live="polite" className="mt-2 text-sm text-red-500">
+              {state.errors.status.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
